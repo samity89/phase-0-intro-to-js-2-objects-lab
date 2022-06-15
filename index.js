f24cd9948702d1a56a30f13a80dcd213b8e050a0
Samuel Amity <sam.amity@gmail.com>
2022-06-15 10:28:36 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: "Ludwig",
+    streetAddress: "the moon",
+};
+
+function updateEmployeeWithKeyAndValue(){
+    return {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(){
+    let newEmployee = {...employee}
+    delete newEmployee.name;   
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    let newEmployee = employee
+    delete newEmployee.name;
+    return newEmployee;
+}
